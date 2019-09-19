73d064473a728f3bbad06ab3bfb4e7c70efcec89
wangjunling <965902682@qq.com>
2019-09-19 17:41:19 +0800

    更新了文件内容

--- src/com/itheima/domain/Test2.java
+++ src/com/itheima/domain/Test2.java
@@ -2,6 +2,6 @@ package com.itheima.domain;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("hello! 王俊令");
     }
 }
