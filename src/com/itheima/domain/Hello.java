b94b96158bc276e77d25425cd7f61fad861c291a
wangjunling88 <44905269+wangjunling88@users.noreply.github.com>
2019-09-19 17:25:13 +0800

    Update Hello.java

--- src/com/itheima/domain/Hello.java
+++ src/com/itheima/domain/Hello.java
@@ -3,6 +3,6 @@ package com.itheima.domain;
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("hello!");
+        System.out.println("hello!12345566");
     }
 }
